5b840a460326bf71b9361c516cf4e87c53528bc4
chandrakanthrck <chandrakanthrajesh@Chandrakanths-MacBook-Pro-2.local>
2024-10-07 14:16:09 -0700

    Three sum

---
+++ two_pointer/ThreeSum.java
@@ -0,0 +1,54 @@
+package two_pointer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Problem Statement: Given an array nums of n integers, find all unique triplets (a, b, c)
+in the array which gives a + b + c = 0.
+
+How to Approach
+Sort the array and use three pointers: a fixed pointer i and two additional pointers (left and right).
+For each value at i, use two pointers (left starts from i+1 and right starts from the end) to find combinations.
+ */
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = threeSum(nums);
+        System.out.println("Unique Triplets: " + result);
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
