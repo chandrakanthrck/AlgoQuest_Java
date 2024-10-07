487b4214b7dc1d91bf065ee51d7600a69945ddde
chandrakanthrck <chandrakanthrajesh@Chandrakanths-MacBook-Pro-2.local>
2024-10-07 16:06:37 -0700

    tree bfs

---
+++ tree_bfs/TreeNode.java
@@ -0,0 +1,12 @@
+package tree_bfs;
+
+public class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    // Constructor to create a new node
+    TreeNode(int val) {
+        this.val = val;
+        left = right = null;
+    }
+}
