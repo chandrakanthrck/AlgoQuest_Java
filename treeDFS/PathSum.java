1b2b61387c8e3488aab0a640a38525e0e3825702
chandrakanthrck <chandrakanthr789@gmail.com>
2024-10-17 19:14:07 -0700

    Practice hasPathSum

--- treeDFS/PathSum.java
+++ treeDFS/PathSum.java
@@ -72,7 +72,6 @@ public class PathSum {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) return false;
 
-        // If it's a leaf node, check if it completes the sum
         if (root.left == null && root.right == null) {
             return targetSum == root.val;
         }
